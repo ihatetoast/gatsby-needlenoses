4c398f0ceac9c6940b82ac34207c0f85993d058b
Katy <ihatetoast@gmail.com>
2018-11-21 23:20:57 -0600

    itty bitty initty. basic gatsby set up. some lorem-filled pages added.

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout';
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Needlenoses</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        consequatur quo aperiam amet, totam incidunt. Vel, animi in tempore
+        explicabo minima consequatur! Omnis, sit animi. Rem nihil odit error
+        atque!
+      </p>
+    </div>
+  </Layout>
+);
+
+export default About;
---
+++ src/pages/groups/galt.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../../components/layout';
+const Galt = () => (
+  <Layout>
+    <div>
+      <h1>Greyhound Adoption League of Texas (GALT)</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        consequatur quo aperiam amet, totam incidunt. Vel, animi in tempore
+        explicabo minima consequatur! Omnis, sit animi. Rem nihil odit error
+        atque!
+      </p>
+    </div>
+  </Layout>
+);
+
+export default Galt;
